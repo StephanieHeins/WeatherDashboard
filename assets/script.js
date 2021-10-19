d482fbf239cf8d040d35c6a829da2a7f546d4a73
Steph Heins <stephanie_heins@yahoo.com>
2021-10-19 00:53:08 -0700

    Finished layout, started on js

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+// API Key 
+var APIKey = "38dcd24915d0b92b98330133a904b9e1"
+
+//Global variables 
+var city = $('#cityinput').val();
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
+
+// Fetch current weather data 
+
+
+// Fetch forecast weather data 
+
+
+// Fetch UV Index data 
+
+
+// Search function 
+
+
+// Save city search in LocalStorage 
+
+
+// Render list of previous searches 
+
+
+// Clear search history 
+
+
+
+
