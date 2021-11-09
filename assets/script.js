de61eafcedae7d143a484ee695ccf1ff13266c4b
Steph Heins <stephanie_heins@yahoo.com>
2021-11-09 14:10:01 -0800

    Added some global variables to script & ids to index

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,22 @@
 var APIKey = "c01d97f095828b431dc2973dc4fc0324"
 
 //Global variables 
-//var city = $('#cityinput').val(); --- This was not working for some reason so changed to use regular JS
-var city = document.querySelector("#cityinput");
 var button = document.querySelector('.button');
+var cityInput = document.querySelector('#cityInput');
+var cityName = document.querySelector('#cityName');
+var tempId = document.querySelector('#tempId');
+var windId = document.querySelector('#windId');
+var humidId = document.querySelector('#humidId');
+var uvId = document.querySelector('#uvId');
+
 
 
 // Fetch current weather data 
 button.addEventListener('click', function(){
   console.log("Test");
   event.preventDefault();
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + APIKey;
+  let city = $('#cityinput').val();
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
   fetch(queryURL)
   .then(response => response.json())
   .then(data => console.log(data))
