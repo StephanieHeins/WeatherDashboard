a0e41cd088d9909a5fe04921aa02f5dc513540f8
Steph Heins <stephanie_heins@yahoo.com>
2021-11-09 17:50:08 -0800

    Worked on the search history

--- assets/script.js
+++ assets/script.js
@@ -27,12 +27,13 @@ button.addEventListener('click', function(){
   console.log("Test");
   event.preventDefault();
   let city = $('#cityinput').val();
+
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
   fetch(queryURL)
   .then(response => response.json())
   .then(data => {
     console.log(data)
-    saveCity(city);
+    saveCity();
     // JSON Data 
     var nameVal = data.name;
     var tempVal = Math.floor(data.main.temp);
@@ -72,7 +73,7 @@ button.addEventListener('click', function(){
             }
         })
   })
-  
+
     // Fetch forecast data from API: https://openweathermap.org/forecast16
     let fcQueryURL = "api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=5&appid=" + APIKey;
     fetch(fcQueryURL)
@@ -80,42 +81,25 @@ button.addEventListener('click', function(){
     .then(data => {
       console.log(data)
   
-    })
+      })
+
     .catch(err => alert("Please enter a valid city name."))
   })
 
-/*
-// Fetch forecast weather data 
-var renderForecast = (event) => {
+// City Search History 
+var saveCity = () => {
   let city = $('#cityinput').val();
-  let fcQueryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-  fetch(fcQueryURL)
-  .then(response => response.json())
-  .then(response => {
-    console.log(response)
-
-  })
+    localStorage.setItem("cities", city);
+   // var history = $("<ul>").addClass("list-group list-group-flush").text(localStorage.getItem("cities"))
+   for (let i = 0; i < localStorage.length; i++) {
+    let cityEl = localStorage.getItem("cities" + i);
+    $("#historylist").prepend(cityEl);
 }
-*/
-
-// Save city search in LocalStorage 
-var saveCity = (newCity) => {
-  let cityExists = false;
-  // Check if exists 
-  for (let i = 0; i < localStorage.length; i++) {
-      if (localStorage["cities" + i] === newCity) {
-          cityExists = true;
-          break;
-      }
-  }
-  // If new, save to LocalStorage 
-  if (cityExists === false) {
-      localStorage.setItem('cities' + localStorage.length, newCity);
-  }
 }
-
-// Render list of previous searches 
-
 
 // Clear search history 
+$("#clear").on("click", (event) => {
+  localStorage.clear();
+  saveCity();
+})
 
